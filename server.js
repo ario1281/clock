54689ebac1f9393573fe68fce49a623203c1c41c
一 龍桜(にのまえ りお) <58287542+ario1281@users.noreply.github.com>
2025-05-18 02:06:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,4 +9,9 @@ async function hander(request) {
       headers: { "Content-type": "text/html" },
     })
   }
+
+  return new Response("404 Not Found", {
+    status: 404,
+    statusText: "Not Found",
+  });
 }
