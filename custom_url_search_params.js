e2807a8855ccf3698a0a6b41a60aeebe56bbd284
一 龍桜(にのまえ りお) <58287542+ario1281@users.noreply.github.com>
2025-05-18 02:32:11 +0900

    Create custom_url_search_params.js

---
+++ custom_url_search_params.js
@@ -0,0 +1,15 @@
+// https://github.com/ryo-ma/github-profile-trophy/blob/master/src/utils.ts
+
+export class CustomURLSearchParams extends URLSearchParams {
+  getString(key, defaultValue = "") {
+    return super.get(key)?.toString() ?? defaultValue;
+  }
+  getNumber(key, defaultValue = 0): {
+    const parsedValue = parseInt(this.getString(key));
+    return !isNaN(parsedValue) ? parsedValue : defaultValue;
+  }
+  getBoolean(key, defaultValue = false) {
+    const param = this.getString(key);
+    return param === "true" ? true : param === "false" ? false : defaultValue;
+  }
+}
