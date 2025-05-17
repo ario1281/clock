a74ae3805a90034211d2a691eae30346b576e03d
一 龍桜(にのまえ りお) <58287542+ario1281@users.noreply.github.com>
2025-05-18 02:04:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 
+
+async function hander(request) {
+  const { pathName, searchParams } = new URL(request.url);
+  console.log({ pathName, searchParams: [...searchParams.entries()] });
+
+  if (pathName === "/") {
+    return new Responce(await Deno.readFile("./template.html"), {
+      headers: { "Content-type": "text/html" },
+    })
+  }
+}
